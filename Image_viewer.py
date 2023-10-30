e17be571ac14d1a1ad56662dedbeac5cf36f2f20
rsrahulak0 <138471226+rsrahulak0@users.noreply.github.com>
2023-10-30 14:55:00 +0530

    Add files via upload

---
+++ Image_viewer.py
@@ -0,0 +1,31 @@
+from tkinter import *
+from tkinter import filedialog
+import tkinter as tk
+from PIL import Image, ImageTk
+import os
+
+def showimage():
+    filename = filedialog.askopenfilename(initialdir=os.getcwd(), title="Select Image", filetypes=(("JPG File", "*.jpg"),("PNG file", "*.png"), ("All file", "how are you .txt")))
+    img = Image.open(filename)
+    img = ImageTk.PhotoImage(img)
+    lbl.configure(image=img)
+    lbl.image = img
+
+root = Tk()
+
+fram = Frame(root)
+fram.pack(side=BOTTOM, padx=15, pady=15)
+
+lbl = Label(root)
+lbl.pack()
+
+btn = Button(fram, text='Select Image', command=showimage)
+btn.pack()
+
+btn2 = Button(fram, text='Exit', command=lambda:exit())
+btn2.pack(side=tk.LEFT, padx=12)
+
+root.title("Image Viewer")
+root.geometry("400x450")
+root.mainloop()
+
